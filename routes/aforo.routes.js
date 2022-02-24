d6259851b48833ed3202a4c1d61ef4f551d7df6f
Fabian Arenas <arenasfabian83@gmail.com>
2022-02-23 20:48:44 -0500

    cors

--- routes/aforo.routes.js
+++ routes/aforo.routes.js
@@ -3,7 +3,7 @@ const route = express.Router();
 const cors = require('cors');
 const controller = require('../controllers/aforo.controller')
 
-var whitelist = ['https://control-de-aaforo.000webhostapp.com/','201.221.173.46']
+var whitelist = ['https://control-de-aaforo.000webhostapp.com/']
 var corsOptions = {
   origin: function (origin, callback) {
     if (whitelist.indexOf(origin) !== -1) {
@@ -14,7 +14,7 @@ var corsOptions = {
   }
 }
 
-route.get('/obtenerAforo', controller.obtenerDatosAforo);
+route.get('/obtenerAforo',cors(corsOptions), controller.obtenerDatosAforo);
 route.post('/actualizarAforo',cors(corsOptions), controller.actualizarAforo);
 route.get('/aumentarCuposDisponibles',cors(corsOptions), controller.aumentarCuposDisponibles);
 route.get('/disminuirCuposDisponibles',cors(corsOptions), controller.disminuirCuposDisponibles);
