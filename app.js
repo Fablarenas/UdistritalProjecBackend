945072044afb10bb4e51fb427429201871f2a24b
Fabian Arenas <arenasfabian83@gmail.com>
2022-02-23 20:41:16 -0500

    cors

--- app.js
+++ app.js
@@ -13,10 +13,10 @@ app.use(bodyParser.json());
 // Configuracion cabeceras y CORS
 // Siempre que se haga una peticion se ejecutara esto primero
 app.use((req, res, next) => {
-    // res.header('Access-Control-Allow-Origin', '*');
-    // res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-    // res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    // res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Access-Control-Allow-Origin');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
     next();
 });
 //rutas
