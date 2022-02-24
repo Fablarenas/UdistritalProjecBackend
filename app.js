766f0c7d956ad6cfdf1ce8d3f56fbbb2774494a4
Fabian Arenas <arenasfabian83@gmail.com>
2022-02-23 21:19:19 -0500

    cors prueba 2

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 // Configuracion cabeceras y CORS
 // Siempre que se haga una peticion se ejecutara esto primero
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin','https://control-de-aaforo.000webhostapp.com/');
+    res.header('Access-Control-Allow-Origin','201.221.173.46');
     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
