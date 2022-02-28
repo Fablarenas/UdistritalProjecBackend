62329a6addc9646b65fed0162e312b1156d880c0
Fabian Arenas <arenasfabian83@gmail.com>
2022-02-27 20:08:48 -0500

    algo

--- routes/aforo.routes.js
+++ routes/aforo.routes.js
@@ -7,5 +7,5 @@ route.get('/obtenerAforo', controller.obtenerDatosAforo);
 route.post('/actualizarAforo', controller.actualizarAforo);
 route.get('/aumentarCuposDisponibles', controller.aumentarCuposDisponibles);
 route.get('/disminuirCuposDisponibles', controller.disminuirCuposDisponibles);
-
+route.get('/ipcliente', (req,res) => res.status(200).send(req.ip));
 module.exports = route;
