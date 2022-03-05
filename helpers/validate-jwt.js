3fde006883e4a03d25e4471b7affb86d359f3d99
Fabian Arenas <arenasfabian83@gmail.com>
2022-03-04 21:45:30 -0500

    token

--- helpers/validate-jwt.js
+++ helpers/validate-jwt.js
@@ -7,13 +7,13 @@ const validarJWT = (req = request , res =response,next) => {
 const token = req.header('x-token');
 
 if (!token) {
-    res.status(404).send({error:"no se encontro ningun token"})
+    res.status(404).send({error:"no se encontro ningun token"});
 }
 try {
     jwt.verify(token,process.env.secretOrPrivateKey);
     next();
 } catch (error) {
-    res.status(404).send({error:"Token invalido"});
+    res.status(401).send({error:"Token invalido"});
 }
 }
 module.exports = validarJWT
